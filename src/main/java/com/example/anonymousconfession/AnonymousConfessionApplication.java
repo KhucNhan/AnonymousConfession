0bfb87f17261c732bbbe22aafb56c370d6f87115
Khuc Nhan <khucvii@gmail.com>
2025-08-13 23:05:37 +0700

    first commit

---
+++ src/main/java/com/example/anonymousconfession/AnonymousConfessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.anonymousconfession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousConfessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymousConfessionApplication.class, args);
+    }
+
+}
